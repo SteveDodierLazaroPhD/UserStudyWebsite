05e97b24a136f48e0673927007b9b2f7798b9b07
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2015-03-18 16:47:18 +0000

    Refactored DataUploadJob (now supports resuming) and integrated it with Doctrine Refactored DataUploadType to include hidden options in the form (representing the existing job) Added a second submit button to DataUploadType to indicate the existing job must be erased
    original bzr status:
    revno: 30
    committer: Steve DODIER-LAZARO <sdodierl@secdev.cs.ucl.ac.uk>
    branch nick: multitasking
    timestamp: Wed 2015-03-18 16:47:18 +0000

--- src/UCL/StudyBundle/Form/Type/DataUploadType.php
+++ src/UCL/StudyBundle/Form/Type/DataUploadType.php
@@ -10,11 +10,22 @@ class DataUploadType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-      $builder->add('file', 'file', array(
-        'label' => 'Data Collection Archive',));
+      //$builder->add('file', 'file', array(
+      //  'label' => 'Data Collection Archive',));
+      
+      $builder->add('participant', 'hidden');
+      $builder->add('part', 'hidden');
+      $builder->add('step', 'hidden');
+      $builder->add('filename', 'hidden');
+      $builder->add('dayCount', 'hidden');
+      $builder->add('expectedSize', 'hidden');
+      $builder->add('obtainedSize', 'hidden');
 
       $builder->add('dataupload', 'submit', array(
-        'label' => 'Upload your Data',));
+        'label' => 'Upload your Data',)); // Managed in the Twig file!
+
+      $builder->add('erasecurrentstartnew', 'submit', array(
+        'label' => 'New Upload (erase previous job)',)); // Managed in the Twig file!
     }
     
     public function getName()
